5dd53dbb72be8da14460416bee9cc3a59a26f8ca
Victor Shulpov <vshulpov@list.ru>
2020-09-15 13:57:34 +0700

    all functions added

--- programming_labs/lab.c
+++ programming_labs/lab.c
@@ -6,6 +6,7 @@
 #include <locale.h>
 #include <Windows.h>
 #include <malloc.h>
+
 #define MAX_BUFF_SIZE 50
 
 typedef struct car_type {
@@ -17,37 +18,122 @@ typedef struct car_type {
 	int benzine;
 };
 
-struct car_type init();
+struct car_type init(); //init
 struct car_type setDataCar(struct car_type car);
+void displayDataCar(struct car_type car);
+struct car_type addBenzine(struct car_type car, int liters);
+struct car_type startEngine(struct car_type car);
+struct car_type stopEngine(struct car_type car);
+struct car_type addSpeed(struct car_type car);
+struct car_type reduceSpeed(struct car_type car);
 
 int main(void)
 {
 	struct car_type car = init();
-	printf("%s\n", car.name);
+	displayDataCar(car);
 	car = setDataCar(car);
-	printf("%s\n", car.name);
+	displayDataCar(car);
+	car = startEngine(car);
+	displayDataCar(car);
+	car = addBenzine(car, 10);
+	displayDataCar(car);
+	car = startEngine(car);
+	displayDataCar(car);
+	for(int i=0; i<4; i++) {
+		car = addSpeed(car);
+		displayDataCar(car);
+	}
+	for (int i = 0; i < 5; i++) {
+		car = reduceSpeed(car);
+		displayDataCar(car);
+	}
+	car = stopEngine(car);
+	displayDataCar(car);
+
 
 	return 0;
 }
 
 struct car_type init() {
 	struct car_type car = { "0", 0, "0", 0, 0, 0 };
-	printf("Car initialized\n");
+	printf("Car initialized!\n");
 	return car;
 
 }
 
 struct car_type setDataCar(struct car_type car) {
 	printf("Car parameters set!\n");
-	printf("Enter a car name: ");
+	printf("\tEnter a car name: ");
 	fflush(stdin);
-	scanf("%s", car.name);
-	printf("Enter a car price: ");
-	fflush(stdin);
-	scanf("%d", car.price);
-	printf("Enter a car color: ");
-	fflush(stdin);
+	scanf("%s", &car.name);
+	printf("\tEnter a car price: ");
+	while (getchar() != '\n');
+	scanf("%d", &car.price);
+	printf("\tEnter a car color: ");
+	while (getchar() != '\n');
 	scanf("%s", car.color);
+	return car;
+}
+
+void displayDataCar(struct car_type car) {
+	puts("\tCar data");
+	printf("\t\tName:\t%s\n", car.name);
+	printf("\t\tPrice:\t%d\n", car.price);
+	printf("\t\tColor:\t%s\n", car.color);
+	printf("\t\tEngine RPM:\t%d\n", car.engineRPM);
+	printf("\t\tSpeed:\t%d\n", car.speed);
+	printf("\t\tBenzine:\t%d\n", car.benzine);
+}
+
+struct car_type addBenzine(struct car_type car, int liters) {
+	printf("%d lit. benzine added!\n", liters);
+	car.benzine += liters;
+	return car;
+}
+
+struct car_type startEngine(struct car_type car) {
+	if (car.benzine > 0) {
+		car.engineRPM = 800;
+		printf("Engine started!\n");
+	}
+	else {
+		printf("No benzine. Engine didn't start!\n");
+	}
+	return car;
+}
+
+struct car_type stopEngine(struct car_type car) {
+	if (car.engineRPM > 0) {
+		car.engineRPM = 0;
+		printf("Engine stopped!\n");
+	}
+	else {
+		printf("Engine stopped already!\n");
+	}
+	return car;
+}
+
+struct car_type addSpeed(struct car_type car) {
+	int speed = 20;
+	if (car.engineRPM > 0) {
+		car.speed += speed;
+		printf("Car speeded up!\n");
+	}
+	else {
+		printf("Engine isn't starting. Car didn't speed up!\n");
+	}
+	return car;
+}
+
+struct car_type reduceSpeed(struct car_type car) {
+	int speed = 20;
+	if (car.speed > 0) {
+		car.speed -= speed;
+		printf("Car speeded down!\n");
+	}
+	else {
+		printf("Car is parking. Car didn't speed down!\n");
+	}
 	return car;
 }
 
