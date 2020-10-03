08dc5c87c0df2299414d649c496f6c29c279aa15
Victor Shulpov <vshulpov@list.ru>
2020-10-03 14:27:17 +0700

    a class creating, init func

--- lab3/lab3/lab3.cpp
+++ lab3/lab3/lab3.cpp
@@ -1,10 +1,68 @@
-﻿// lab3.cpp
-//
-
+﻿// lab work №2 Shulpov Victor PI-92
+/*
+Реализовать работу автомобиля на примере динамической структуры.
+Поля структуры:
+•	Название (строка)
+•	Цена (целое)
+•	Цвет (строка)
+•	Количество оборотов двигателя в минуту (целое)
+•	Скорость (целое)
+•	Количество бензина (целое)
+Функции:
+•	Инициализация
+•	Установка параметров автомобиля
+•	Вывод данных машины
+•	Запуск двигателя
+•	Остановка двигателя
+•	Добавление скорости
+•	Уменьшить скорость
+*/
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <conio.h>
+
+#define CAR_NUMBERS 100
+
+
+class Car
+{
+public:
+	void init(char* name, int price, char* color, int engineRPM, int speed, int benzine); //init
+	char* name;
+
+private:
+	
+	int price;
+	char* color;
+	int engineRPM;
+	int speed;
+	int benzine;
+};
+
+Car* car = new Car;
+
+void Car::init(char* name, int price, char* color, int engineRPM, int speed, int benzine)
+{
+	this->name = name;
+	this->price = price;
+	this->color = color;
+	this->engineRPM = engineRPM;
+	this->benzine = benzine;
+	this->speed = speed;
+	printf("Car initialized!\n");
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	char name[] = "", color[] = "";
+	strcat(name,"BMW");
+	strcat(color, "red");
+	car->init(name, 100, color, 0, 0, 0);
+
+    std::cout << car[0].name <<"\n";
+
+	_getch();
+	return 0;
 }
 
