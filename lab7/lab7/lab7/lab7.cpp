66c8d8be82f541afeac283a14444ad0752201de1
Victor Shulpov <vshulpov@list.ru>
2020-10-29 18:23:08 +0700

    lab7: return via poiter and link

---
+++ lab7/lab7/lab7/lab7.cpp
@@ -0,0 +1,36 @@
+﻿// lab7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <array>
+#include <iostream>
+
+int* allocateArray(int size);
+int& getElement(int& array, int index);
+int main()
+{
+    //возврат значения из метода через указатель(*)
+    int* myArray = allocateArray(20);
+    // делаем что-нибудь с array
+    delete[] myArray;
+    //возврат значения из метода через ссылку(&)
+    int myArray2[5] = {1,2,3,4,5};
+    int &linkMyArray = myArray2[0];
+    // присваиваем элементу массива с индексом 15 значение 7
+    getElement(linkMyArray, 3) = 7;//содержимое myArray2 : {1,2,3,7,5};
+    return 0;
+}
+
+
+int* allocateArray(int size)
+//функция выделения памяти (возврат значения по адресу(указателю))
+{
+    return new int[size];
+}
+
+
+int& getElement(int &array, int index)
+// Возвращаем ссылку на индекс элемента массива
+{
+    // мы знаем, что array[index] не уничтожится, когда мы будем возвращать данные в caller (так как caller сам передал этот array в функцию!)
+    // так что здесь не должно быть никаких проблем с возвратом по ссылке 
+    return *(&array+index);
+}
